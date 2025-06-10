deba3a21043c3294d6613135bacac89db44f7009
Dina Knyazeva <immortlais.mors0@gmail.com>
2025-06-10 21:48:09 +0300

    add headless1

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -40,6 +40,7 @@ public class BaseTest {
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-infobars");
+        options.addArguments("--headless");
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
